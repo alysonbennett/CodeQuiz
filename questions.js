b399c0b624589d9816aae81060cfb310a83847c4
Alyson Bennett <alysonbennett@gmail.com>
2020-10-04 23:17:15 -0400

    adding code, still not working

--- questions.js
+++ questions.js
@@ -33,17 +33,18 @@ var score;
 var timeLeft = 75;
 var timer;
 var penalty = 10;
-var countdownClock = document.querySelector("#CountdownClock")
+var countdownClock = document.querySelector("#CountdownClock");
 var quizBody = document.querySelector("#quizBody");
-var choicesList = document.createElement("choicesUL")
+var choicesList = document.createElement("#choicesUL");
+var startButton = document.querySelector("#startQuiz");
 
 // When the user clicks the "Start Quiz" button, start timer at 75 seconds that is displayed at the top right of the screen
 
-countdownClock.addEventListener("click", function() {
+startButton.addEventListener("click", function() {
 
        var timerInterval = setInterval(function() {
            secondsLeft--;
-           countdownClock.textContent = secondsLeft
+           countdownClock.textContent = secondsLeft;
 
            if(secondsLeft === 0) {
                clearInterval(timerInterval);
@@ -63,15 +64,68 @@ function startQuestions () {
     choicesList.innerHTML = "";
     // For loop to go through questions array
     for (var i = 0; i < questions.length; i++) {
+    // Append question and choices
+        var userQuestions = questions.title;
+        var userChoices = questions.choices;
+        quizBody.textContent = userQuestions;
+
         
+
+        // I know this aint right but my brain hurt.....
+
+        choicesList createElement("li")
+        questions appendChild
+        somethingsomething .addEventListener("click", (compare))
     }
 }
 
-// If user selects the right answer, show comment "Correct!" and move onto next question
+function compare(event) {
+// If the users choice matches the answer, return "Correct!" and bottom of div and move on to next question
+    var sure = event.target;
+    if (sure.matches(//this thing)) {
+        //some shit
+        createElement.textContent = ("Correct!");
+    }
+    
+    // If the users choice does not match, return "Incorrect" at bottom of div, subtract 10 seconds from timer, and move onto next qustion
 
-// If user selects the wrong answer, show comment "Incorrect", subract 10 seconds from the timer and move onto the next question
+    else {
+        timer = timeLeft - penalty;
+        createElement.textContent = ("Nope");
+    }
 
-// After all questions have been answered, display "All done!" with the users final score
 
-// Also, have input line for their intials with a "Submit" button, with two buttons below that say "Go Back" and "Clear Highscores"
+}
+
+// After all questions have been answered, display "All done!" with "Your final score is: " and input box for initials, with submit button
+function endGame () {
+    quizBody.innerHTML = "";
+    choicesList.innerHTML = "";
+    
+    var allDoneH1 = document.createElement("h1");
+    allDoneH1.textContent = ("All Done!");
+
+    var doneScore = document.createElement("p");
+    doneScore.textContent = ("Your final score is: " + timer);
+
+    var enterInitials = document.createElement("input");
+    enterInitials.//how do I create a text box thingy?
+    enterInitials.textContent = //that thing above
+
+    // Submit button... cuz that's a fun thing
+    var submit = document.createElement("button");
+    submit.//some other shit
+    submit.textContent = "Submit"
+
+
+// After submit, display thier score with their initials, and buttons that "Go Back" and "Clear High Scores"
+// bring thru...
+doneScore
+enterInitials
+
+var goBack = document.createElement("goBackButton");
+var clearHighScores = document.createElement("clearHighScores");
+
+//put in your local storage here to link to HighScores.html---do you really need that other js file? Cuz it be cool if ya didnt
+
 
